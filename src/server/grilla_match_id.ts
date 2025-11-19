c19726c42b1368ac989a3afdf303b07d391a1d05
Eugenio <eugenio.arosteguy@gmail.com>
2025-11-19 17:06:58 -0300

    ticket #60 agregar campos solicitados por Valeria

--- src/server/grilla_match_id.ts
+++ src/server/grilla_match_id.ts
@@ -35,10 +35,10 @@ export function grilla_match_id(): TableDefinition {
     
     // Solo se ven campos que pide procesamiento
     def.fields.forEach(f=>f.visible=false); // set all not visible
-    const fieldsToShow = ['idblaise', 'operativo', 'enc', 'msnombrei', 'rea', 'norea', 'rea_web', 'rea_tel', 'rea_pres', 'verif_campo', 'verificado_procesamiento', 'observaciones', 'web_proc', 'resul_proc', 'lote', 'observaciones', 'respid', 
+    const fieldsToShow = ['idblaise', 'operativo', 'enc', 'msnombrei', 'rea', 'norea', 'rea_web', 'rea_tel', 'rea_pres', 'tarea_actual', 'result_sup', 'verif_campo', 'verificado_procesamiento', 'observaciones', 'web_proc', 'resul_proc', 'lote', 'observaciones', 'respid', 
         //int01 ya no está en el nuevo backup (del 29/10 que mandó alex, sacamos de acá en adelante)
         //'int01', 'int02', 'dem01', 'dem02', 'dem03', 'dem06', 'dem07', 'dem08', 'dem09', 'dem11', 'dem14', 'dem21', 'gen01', 'gen02', 'gen11', 'gen25', 'gen44b', 'hhd24a', 'hhd28', 'hhd35', 'wel01', 'wel02', 'wel02a', 'lhi01', 'lhi02', 'att08', 'breportrep01', 'breportrep04', 'breportrep02', 'breportrep06', 'breportrep07', 'complete', 'agreedintro', 'begindate', 'begintime', 'enddate', 'endtime', 'numbiol', 'numstep', 'numadopt', 'totalchildren', 'hhd01b'
-        'interviewer_id', 'interviewer_name', 'b1dem01', 'b1dem02', 'b10hhd01b', 'b11gen01', 'b11gen02', 'numbiol', 'numstep', 'numadopt', 'nkidstotal', 'age', 'partnerage', 'hascorespartner', 'hascoreschildunder15', 'mumcores', 'dadcores', 'adopt_step_fos', 'begindate', 'begintime', 'enddate', 'endtime', 'complete', 'agreedintro'
+        'interviewer_id', 'interviewer_name', 'b1dem01', 'b1dem02', 'b10hhd01b', 'b11gen01', 'b11gen02', 'numbiol', 'numstep', 'numadopt', 'nkidstotal', 'age', 'partnerage', 'hascorespartner', 'hascoreschildunder15', 'mumcores', 'dadcores', 'adopt_step_fos', 'begindate', 'begintime', 'enddate', 'endtime', 'complete', 'last_var', 'agreedintro'
     ]
     
     fieldsToShow.forEach(fNameToShow=>{
